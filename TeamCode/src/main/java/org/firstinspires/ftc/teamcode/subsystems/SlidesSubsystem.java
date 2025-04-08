2d3e162349cd0d7d10166a35506914d3f405acae
ABlueberryC <clairedong9@gmail.com>
2025-04-07 17:41:20 -0700

    slides subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SlidesSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SlidesSubsystem.java
@@ -11,38 +11,45 @@ public class SlidesSubsystem extends SubsystemBase {
     SolversMotor slideLeft, slideRight;
 
     PIDController pidController;
-    private double kP, kI =0, kD = 0;
+    private double kP = 0, kI = 0, kD = 0;
 
-
-    private double powerThreshold = 0.2;
+    public double slidePowerThreshold = 0.1;
 
     public int targetPos = 0;
     public int low = 0;
     public int med = 200;
     public int high = 500;
 
-    public void init(HardwareMap hm){
-        pidController.setPID(0.1, 0.1, 0.1);
+    public SlidesSubsystem(final HardwareMap hm, final String left, final String right) {
+        SolversMotor slideleft = new SolversMotor(hm.get(DcMotorEx.class, left), slidePowerThreshold);
+        SolversMotor slideright = new SolversMotor(hm.get(DcMotorEx.class, right), slidePowerThreshold);
 
-        SolversMotor slideLeft = new SolversMotor(hm.get(DcMotorEx.class, "slideL"), powerThreshold);
-        SolversMotor slideRight = new SolversMotor(hm.get(DcMotorEx.class, "slideR"), powerThreshold);
+        pidController = new PIDController(kP, kI, kD);
 
         slideLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         slideRight.setDirection(DcMotorSimple.Direction.FORWARD);
-
-        pidController  = new PIDController(kP, kI, kD);
-
-        slideLeft.getPosition();
     }
 
-    public void updatePID(){
-        slideLeft.setPower(powerThreshold);
-        slideRight.setPower(powerThreshold);
-
-
+    public void Loop(){
+        double output = pidController.calculate(slideLeft.getPosition(), targetPos); // calculates output needed
+        slideLeft.setPower(output);
+        slideRight.setPower(output);
     }
 
-    public void setTargetPos(int targetPos){
-        this.targetPos = targetPos;
+//  dunno if i need
+//    public void setTargetPos(int targetPos){
+//        this.targetPos = targetPos;
+//    }
+
+    public void setHigh(){
+        targetPos = high;
+    }
+
+    public void setMed(){
+        targetPos = med;
+    }
+
+    public void setLow(){
+        targetPos = low;
     }
 }
