b603d6bc74fd2dac4050775a067102babbadff6d
twenty-one21 <31107399+twenty-one21@users.noreply.github.com>
2025-04-07 17:08:43 -0700

    drive for x

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpCommandOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpCommandOpMode.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.seattlesolvers.solverslib.command.Command;
 import com.seattlesolvers.solverslib.command.CommandOpMode;
+import com.seattlesolvers.solverslib.command.Subsystem;
 
-import org.firstinspires.ftc.teamcode.commands.ScoochForwardCommand;
+import org.firstinspires.ftc.teamcode.commands.DriveForXCommand;
 import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.SlidesSubsystem;
 
@@ -11,14 +13,45 @@ import org.firstinspires.ftc.teamcode.subsystems.SlidesSubsystem;
 public class TeleOpCommandOpMode extends CommandOpMode {
     private DriveSubsystem drive;
     private SlidesSubsystem slides;
-    private ScoochForwardCommand scoochForwardCommand;
+    private DriveForXCommand scoochForwardCommand;
     @Override
     public void initialize() {
-        //Initializing hardware
+        //Initialize hardware
         DriveSubsystem drive = new DriveSubsystem(hardwareMap, "leftFront", "leftRear", "rightFront", "rightRear");
         SlidesSubsystem slides = new SlidesSubsystem(hardwareMap, "slideL", "slideR");
 
-        //Scheduling commands
+        //Schedule commands
         schedule();
     }
+    @Override
+    public void run() {
+        super.run();
+    }
+
+    @Override
+    public void schedule(Command... commands) {
+        super.schedule(commands);
+    }
+
+    @Override
+    public void register(Subsystem... subsystems) {
+        super.register(subsystems);
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        super.runOpMode();
+    }
+
+    @Override
+    public void end() {
+        super.end();
+    }
+
+    @Override
+    public void initialize_loop() {
+        super.initialize_loop();
+    }
+
+
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/DriveForXCommand.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.commands;
+
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.seattlesolvers.solverslib.command.CommandBase;
+import com.seattlesolvers.solverslib.util.Timing;
+
+import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
+
+import java.util.concurrent.TimeUnit;
+
+public class DriveForXCommand extends CommandBase {
+    private final DriveSubsystem drive;
+    private ElapsedTime timer = new ElapsedTime();
+    private int driveTime_ms;
+    private double driveX, driveY, driveRX;
+    public DriveForXCommand(DriveSubsystem drive, double driveX, double driveY, double driveRX, int driveTime_ms) {
+        //Constructing command w/ subsystem
+        this.drive = drive;
+        this.driveX = driveX;
+        this.driveY = driveY;
+        this.driveRX = driveRX;
+        this.driveTime_ms = driveTime_ms;
+        addRequirements(drive);
+    }
+
+    @Override
+    public void initialize() {
+        //Drive in +y and start timer
+        drive.setDriveVectors(0, 1, 0);
+        timer.reset();
+    }
+    @Override
+    public boolean isFinished() {
+        //Return true when timer>driveTime
+        return timer.time(TimeUnit.MILLISECONDS) > driveTime_ms;
+    }
+}
