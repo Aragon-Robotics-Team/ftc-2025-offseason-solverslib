660a058c4863e998ca58290ef6c2c7f6d9271332
twenty-one21 <31107399+twenty-one21@users.noreply.github.com>
2025-04-21 16:51:39 -0700

    bna

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/RobotHardware.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.seattlesolvers.solverslib.solversHardware.SolversMotor;
+
+public class RobotHardware {
+    SolversMotor leftFront;
+    SolversMotor leftRear;
+    SolversMotor rightFront;
+    S
+}
