68f7f59037fd0412b854064b082b5fb3037d0813
twenty-one21 <31107399+twenty-one21@users.noreply.github.com>
2025-04-21 17:52:41 -0700

    sigma

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/GlobalConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/GlobalConstants.java
@@ -1,4 +1,7 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
-public class GlobalConstants {
+public final class GlobalConstants {
+    public final static double SLIDES_HIGHPOS = 1000;
+    public final static double SLIDES_MEDPOS = 500;
+    public final static  double SLIDES_LOWPOS = 0;
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
@@ -1,9 +1,7 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.HardwareMap;
+
 import com.seattlesolvers.solverslib.command.SubsystemBase;
-import com.seattlesolvers.solverslib.solversHardware.SolversMotor;
 
 import org.firstinspires.ftc.teamcode.hardware.RobotHardware;
 
