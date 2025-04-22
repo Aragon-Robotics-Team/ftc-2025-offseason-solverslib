03b628278e303764b9afc878d9ab53c843faa37a
twenty-one21 <31107399+twenty-one21@users.noreply.github.com>
2025-04-21 17:17:33 -0700

    lock in

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpCommandOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpCommandOpMode.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import android.transition.Slide;
 
+import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.seattlesolvers.solverslib.command.Command;
 import com.seattlesolvers.solverslib.command.CommandOpMode;
@@ -15,11 +16,13 @@ import org.firstinspires.ftc.teamcode.commands.DriveForXCommand;
 import org.firstinspires.ftc.teamcode.commands.SlidesHigh;
 import org.firstinspires.ftc.teamcode.commands.SlidesLow;
 import org.firstinspires.ftc.teamcode.commands.SlidesMed;
+import org.firstinspires.ftc.teamcode.hardware.RobotHardware;
 import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.SlidesSubsystem;
 
 @TeleOp
 public class TeleOpCommandOpMode extends CommandOpMode {
+    private RobotHardware robot;
     private DriveSubsystem drive;
     private SlidesSubsystem slides;
     private DriveForXCommand driveForXCommand;
@@ -30,6 +33,7 @@ public class TeleOpCommandOpMode extends CommandOpMode {
     GamepadEx gp2 = new GamepadEx(gamepad2);
     @Override
     public void initialize() {
+        robot.init(hardwareMap);
         super.reset(); //Reset command scheduler
 
         //Initialize hardware
@@ -45,5 +49,9 @@ public class TeleOpCommandOpMode extends CommandOpMode {
         );
 
         slidesUpButton.whenPressed(new SlidesHigh(slides)).whenReleased(new SlidesLow(slides));
+    }
+    public void run() {
+        telemetry.update();
+        robot.all
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/RobotHardware.java
@@ -1,11 +1,54 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.seattlesolvers.solverslib.solversHardware.SolversMotor;
 
+import java.util.List;
+
 public class RobotHardware {
-    SolversMotor leftFront;
-    SolversMotor leftRear;
-    SolversMotor rightFront;
-    S
+    public SolversMotor leftFront;
+    public SolversMotor leftRear;
+    public SolversMotor rightFront;
+    public SolversMotor rightRear;
+    public SolversMotor slideLeft;
+    public SolversMotor slideRight;
+    private static RobotHardware instance = new RobotHardware();
+    public static RobotHardware getInstance() {
+        if (instance == null) {
+            instance = new RobotHardware();
+        }
+        return instance;
+    }
+    public void init(HardwareMap hm) {
+
+        leftFront = new SolversMotor(hm.get(DcMotorEx.class, "lf"), 0.01);
+        leftRear = new SolversMotor(hm.get(DcMotorEx.class, "lr"), 0.01);
+        rightFront = new SolversMotor(hm.get(DcMotorEx.class, "rf"), 0.01);
+        rightRear = new SolversMotor(hm.get(DcMotorEx.class, "rr"), 0.01);
+        slideLeft = new SolversMotor(hm.get(DcMotorEx.class, "slideLeft"), 0.01);
+        slideRight = new SolversMotor(hm.get(DcMotorEx.class, "slideRight"), 0.01);
+
+        leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); //Run without velocity PID control
+        leftRear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightRear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        slideLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        slideRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        List<LynxModule> allHubs = hm.getAll(LynxModule.class);
+
+        for (LynxModule hub : allHubs) {
+            hub.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL);
+        }
+
+
+    }
 }
